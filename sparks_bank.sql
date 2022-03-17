ba21566af246f19ccec35594f7538010ab227f28
OmkarNarvekar001 <onarvekar01@gmail.com>
2022-03-17 17:44:13 +0530

    BankingSystem

--- sparks_bank.sql
+++ sparks_bank.sql
@@ -5,12 +5,12 @@ CREATE TABLE `transaction` (
   `receiver` text NOT NULL,
   `balance` int(8) NOT NULL,
   `datetime` datetime NOT NULL DEFAULT current_timestamp()
-) 
+);
 
 -- Inserting data for table `transaction`
 INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
 (2, 'Riddhi', 'Shivam', 9000, '2021-05-02 18:28:06'),
-(3, 'Omkar', 'Aman', 25000 '2021-05-18 10:15:43'),
+(3, 'Omkar', 'Aman', 25000, '2021-05-18 10:15:43'),
 (4, 'Juhi', 'Priya', 3000, '2021-05-10 07:29:34');
 
 -- Table structure for table `users`
@@ -19,7 +19,7 @@ CREATE TABLE `users` (
   `name` text NOT NULL,
   `email` varchar(30) NOT NULL,
   `balance` int(8) NOT NULL
-)
+);
 
 -- Dumping data for table `users`
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
@@ -28,8 +28,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
 (3, 'Nimesh', 'nimehshah@yahoo.com', 36030),
 (4, 'Priya', 'priya12@gmail.com', 10876),
 (5, 'Shivam', 'shivam@yahoo.com', 40000),
-(6, 'Ravi', 'singhravi@gmail.com', 19923);
-(7, 'Deepak', 'deepak90@hotmail.com', 51961)
+(6, 'Ravi', 'singhravi@gmail.com', 19923),
+(7, 'Deepak', 'deepak90@hotmail.com', 51961),
 (8, 'Juhi', 'sharmajuhi99@gmail.com', 80105),
 (9, 'Aman', 'aman88@yahoo.com', 21897),
 (10, 'Varun', 'varun67634@gmail.com', 52067);
